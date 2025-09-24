a9dd068da7a3e519fc16bd46e15bb4707f20ac39
dsxbmotw <dsxbmotw@outlook.com>
2025-09-24 19:36:52 +0800

    增加下载功能

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Downloader:
         self.timeout = timeout
         self.session = session
 
-    async def download(self, url: str, path: str, sha1: str = "") -> Path | None:
+    async def download(self, url: str, path: str, sha1: str = "") -> str | None:
         Path(path).parent.mkdir(parents=True, exist_ok=True)
 
         for attempt in range(1, self.retries + 1):
@@ -40,7 +40,7 @@ class Downloader:
                         )
 
                 print(f"下载完成：{path}")
-                return Path(path)
+                return path
 
             except Exception as e:
                 print(f"【第 {attempt}/{self.retries} 次尝试】下载失败：{url}\n原因：{e}")
@@ -66,10 +66,16 @@ async def get_version_dict_list(version_manifest_v2_path):
 
 
 async def get_version_list(version_dict_list):
+    release_version_list = []
+    snapshot_version_list = []
     version_list = []
     for version in version_dict_list:
+        if version["type"] == "snapshot":
+            snapshot_version_list.append(version["id"])
+        elif version["type"] == "release":
+            release_version_list.append(version["id"])
         version_list.append(version["id"])
-    return version_list
+    return snapshot_version_list, release_version_list, version_list
 
 
 async def get_selected_version(version_list, version_dict_list):
@@ -80,20 +86,38 @@ async def get_selected_version(version_list, version_dict_list):
                 return version
     raise ValueError(f"输入的版本号无效：{selected_version_str}")
 
+async def download_assets_of_selected_version(selected_version_json, minecraft_folder):
+    assets_folder = os.path.join(minecraft_folder, "assets")
+
+async def download_selected_version(dl: Downloader, selected_version, minecraft_folder, version_isolation=False):
+    minecraft_versions_path = os.path.join(minecraft_folder, "versions")
+    selected_version_id = selected_version["id"]
+
+    selected_version_folder = os.path.join(minecraft_versions_path, selected_version_id)
+    selected_version_json_path = os.path.join(selected_version_folder, f"{selected_version_id}.json")
+    selected_version_json_url = selected_version["url"]
+    selected_version_json_sha1 = selected_version["sha1"]
+    await dl.download(selected_version_json_url, selected_version_json_path, selected_version_json_sha1)
+    async with aiofiles.open(selected_version_json_path, mode="r") as f:
+        selected_version_json = await f.read()
+
+
 
 async def main():
     async with aiohttp.ClientSession() as session:
         cwd = os.getcwd()
         pml_folder = os.path.join(cwd, "PML")
+        minecraft_folder = os.path.join(cwd, ".minecraft")
+        version_isolation = False
         version_manifest_v2_path = os.path.join(pml_folder, "version_manifest_v2.json")
 
         dl = Downloader(session=session)
 
         await download_version_manifest_v2(dl, version_manifest_v2_path)
         latest_version_dict, version_dict_list = await get_version_dict_list(version_manifest_v2_path)
-        version_list = await get_version_list(version_dict_list)
-        selected_version = await get_selected_version(version_list, version_dict_list)
-        print(selected_version)
+        snapshot_version_list, release_version_list, version_list = await get_version_list(version_dict_list)
+        selected_version = await get_selected_version(release_version_list, version_dict_list)
+        await download_selected_version(dl, selected_version, minecraft_folder, version_isolation)
 
 
 if __name__ == "__main__":
